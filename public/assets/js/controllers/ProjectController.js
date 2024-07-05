ddec148c2b65bb4ad9280da0a93267cd16decebc
Luke-Fernando <isanjaya139@gmail.com>
2024-07-05 21:54:52 +0530

    project dashboard page is completed

--- public/assets/js/controllers/ProjectController.js
+++ public/assets/js/controllers/ProjectController.js
@@ -20,7 +20,54 @@ class ProjectController extends Controller {
         select.triggerDropdowns();
     }
 
-    projects() { }
+    projects() {
+        this.triggerProjects();
+    }
+
+    expandProject(content) {
+        content.classList.remove("invisible");
+        content.classList.remove("max-h-0");
+        content.classList.add("max-h-[2500px]");
+        content.setAttribute("data-show", "true");
+    }
+
+    collapseProject(content) {
+        content.classList.remove("max-h-[2500px]");
+        content.classList.add("max-h-0");
+        content.classList.add("invisible");
+        content.setAttribute("data-show", "false");
+    }
+
+    toggleProject(content) {
+        let state = content.getAttribute("data-show");
+        if (state == "true") {
+            this.collapseProject(content);
+        } else {
+            this.expandProject(content);
+        }
+    }
+
+    triggerProjects() {
+        const projectTriggers = document.querySelectorAll("[data-project-expand-trigger]");
+
+        projectTriggers.forEach(trigger => {
+            if (trigger != null) {
+                document.addEventListener("click", (event) => {
+                    if (trigger.contains(event.target)) {
+                        let id = trigger.getAttribute("data-project-expand-trigger");
+                        let projectElements = document.querySelectorAll(`[data-project-element="${id}"]`);
+                        projectElements.forEach(element => {
+                            element.addEventListener("click", (event) => {
+                                event.stopPropagation();
+                            });
+                        });
+                        let content = document.querySelector(`[data-project-expand-content="${id}"]`);
+                        this.toggleProject(content);
+                    }
+                });
+            }
+        });
+    }
 
 }
 
